df99015917594eacf42e0637b89a54ede7c4e3e6
erdeiistvan <66384973+erdeiistvan@users.noreply.github.com>
2021-03-02 20:11:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,63 @@
-var mainPage = document.querySelector(".wrapper");
-var pages = Array.from(document.querySelectorAll(".container"));
-var menuBtns = Array.from(document.querySelectorAll(".menu-btn"));
-
-function routerConstructor(name, path) {
-    return {
-        name: name,
-        path: path
-    }
+var domElements = {
+    mainPage : document.querySelector(".wrapper"),
+    pages : Array.from(document.querySelectorAll(".container")),
+    menuBtns : Array.from(document.querySelectorAll(".menu-btn"))
 };
 
-var routerData = new routerConstructor("router", [
+class Router {
+    constructor(name, path) {
+        this.name = name,
+        this.path = path
+    }
+    addRouteInfo(element) {
+        var routeInfo = element.getAttribute("router-link");
+        window.history.pushState({}, "name", routeInfo);
+        
+    }
+    pathfinder() {
+        
+        var currentPath = window.location.hash.slice(1) || "/";
+        var route = Object.values(this.path).filter(function(r){
+            return r.path === currentPath;
+        });
+        if(route.length === 0) {
+            alert("The page does not exist!");
+        } else {
+            var pageToDisplay = Object.values(route)[0].name;
+            this.displayPage(domElements.pages, pageToDisplay);
+        }
+    }
+    displayPage(pageArray, page) {
+        for(var j = 0; j < pageArray.length; j++) {
+            pageArray[j].classList.remove("active");
+        }
+        page.classList.add("active");
+    }
+}
+
+var routerData = new Router("myRouter", [ 
     {
-        path: "/",
-        name: mainPage
+        name : domElements.mainPage,
+        path : "/"
     },
     {
-        path: "/men",
-        name: pages[0]
+        name : domElements.pages[0],
+        path : "/men"
     },
     {
-        path: "/women",
-        name: pages[1]
+        name : domElements.pages[1],
+        path : "/women"
     },
     {
-        path: "/kids",
-        name: pages[2]
+        name : domElements.pages[2],
+        path : "/kids"
     }
 ]);
 
-for(var i = 0; i < menuBtns.length; i++) {
-    menuBtns[i].addEventListener("click", addRouteInfo);
-    menuBtns[i].addEventListener("click", pathFinder);
+for(var i = 0; i < domElements.menuBtns.length; i++) {
+    domElements.menuBtns[i].addEventListener("click", function(e){
+        routerData.addRouteInfo(this);
+        routerData.pathfinder();
+    });
 }
-
-function getCurrentPath() {
-    return window.location.hash.slice(1) || "/";
-};
-
-function pathFinder() {
-    var currentPath = getCurrentPath();
-    var route = Object.values(routerData.path).filter(function(r){
-        return r.path === currentPath
-    })
-    if(route.length === 0) {
-        console.log("Error! Route does not exist!");
-    } else {
-        var pageToShow = Object.values(route)[0].name;
-        showPage(pages, pageToShow);
-    }
-};
-
-pathFinder();
-
-function addRouteInfo() {
-    var routeInfo = this.getAttribute("router-link");
-    window.history.pushState({}, "name", routeInfo);
-};
-
-function showPage(pageArray, page) {
-    for(var x = 0; x < pageArray.length; x++) {
-        pageArray[x].classList.remove("active");
-    }
-    page.classList.add("active");
-};
+routerData.pathfinder();
